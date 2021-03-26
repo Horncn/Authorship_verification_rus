d4a0d05fa2d4987a576d4260d38d349b4002beae
Horncn <55386427+Horncn@users.noreply.github.com>
2021-03-26 08:41:22 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from texts_to_data import Doc2Data
-from texts_to_data import CalculatePair
-from mdl import train_model, get_data
+from text_to_data import Doc2Data
+from text_to_data import CalculatePair
+from model import train_model, get_data
 from progress.bar import Bar
 import os
 from corpy.udpipe import Model
@@ -10,8 +10,8 @@ import numpy as np
 
 m = Model("russian-syntagrus-ud-2.5-191206.udpipe")
 stop = stopwords.get_stopwords('ru')
-# postfix can be _sm for demo corpus, _med for other texts, _all for all corpus
-postfix = '_sm'
+# postfix can be _sm for demo corpus and _med for other texts
+postfix = '40k'
 
 text_folder_name = 'texts' + postfix + '/'
 data_folder_name = 'data' + postfix + '/'
@@ -71,20 +71,13 @@ def load_train_data():
     return a, b
 
 
-def calculate(start=3):
+def calculate(start=3): # function to process every step of preparing dataset and training model
     if start < 2:
         make_data_from_texts()
     if start < 3:
         make_pairs(-1)
     a, b = load_train_data()
-    if start < 4:
-        train_model(a, b)
-    if start == 4:
-        get_data(a, b)
+    train_model(a, b)
 
 
-calculate(1)
-# 1 for process text
-# 2 for process pairs
-# 3 for train model
-# 4 for show confusion matrix and metrics
+
